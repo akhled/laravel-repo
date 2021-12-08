ae2b7f9183a115f7354799409844314f56b73f54
Amro Khaled <wshcylinder@gmail.com>
2021-12-08 21:36:55 +0200

    :tada: Init

---
+++ src/LaravelRepoServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Akhaled\LaravelRepo\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelRepoServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
