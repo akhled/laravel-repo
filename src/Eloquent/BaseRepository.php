901f104c90dc976f495438bf27f3c524dbdc7135
Amro Khaled <wshcylinder@gmail.com>
2021-12-27 13:32:25 +0200

    :zap: Add scope if exists before smart pagination

--- src/Eloquent/BaseRepository.php
+++ src/Eloquent/BaseRepository.php
@@ -6,5 +6,12 @@ use AwesIO\Repository\Eloquent\BaseRepository as BaseBaseRepository;
 
 abstract class BaseRepository extends BaseBaseRepository
 {
-    //
+    public function smartPaginate($perPage = null, $columns = ['*'])
+    {
+        if (method_exists($this, 'scope')) {
+            $this->scope(request());
+        }
+
+        return parent::smartPaginate();
+    }
 }
